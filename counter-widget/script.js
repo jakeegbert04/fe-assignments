0179f339f39d9fa336a5c37d388d0f27a4b18495
Jake Egbert <jake.egbert04@gmail.com>
2023-03-18 22:12:59 -0600

    Finished HTML

---
+++ counter-widget/script.js
@@ -0,0 +1,8 @@
+/* 
+Counter Widget
+ - Header
+ - Current count (start at 0)
+ - Three buttons: decrease, reset, increase, that will update the count and render to the dom respectively.
+ - Current count should black if the count is 0. If it's in the positive, the count should be green, negative should be red.
+ - Style everything
+*/
