4455fdb46fd3438f26135d59af50108a219acee8
Jake Egbert <jake.egbert04@gmail.com>
2023-03-18 19:05:24 -0600

    got basic styling done

---
+++ dynamic-header/script.js
@@ -0,0 +1,14 @@
+/* 
+Dynamic Navbar
+
+- I don't care what content you have in the navbar
+  - ex: logo on the left, links on the right
+- Media queries ot make the navbar responsive
+  - at a particular, display a hamburger icon instead of links
+  - using js, make the hamburger "open" and "close" the menu to show links
+
+Hint: 
+ - start with the pieces (html skeleton, styles, media queries, js)
+ - don't get hung up on 1 piece
+*/
+
