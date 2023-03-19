0d7d0fefa83ec243b38f24b4f2fd9595b0a3f6e7
Jake Egbert <jake.egbert04@gmail.com>
2023-03-18 21:29:45 -0600

    got the icon to switch on click

---
+++ dynamic-header/notes.js
@@ -0,0 +1,24 @@
+/* 
+function iconAddX() {
+  const dropdown = document.getElementsByClassName("navbar-icon");
+
+  
+  const boxPressed = (e) => {
+    if(e.target.className === "fa-bars") {
+      e.target.className = "fa-solid fa-xmark navbar-icon";
+    } else if (e.target.className === "fa-xmark") {
+      e.target.className = "fa-solid fa-bars navbar-icon";
+    }
+  }
+  dropdown[0].addEventListener("click", boxPressed);
+}
+
+// function iconAddBar()
+iconAddX()
+
+
+<div class="dropdown">
+          <p href=""><i class="fa-solid fa-bars navbar-icon"></i></p>
+        </div>
+*/
+
--- dynamic-header/script.js
+++ dynamic-header/script.js
@@ -11,4 +11,43 @@ Hint:
  - start with the pieces (html skeleton, styles, media queries, js)
  - don't get hung up on 1 piece
 */
+// const dropdown = document.getElementsByClassName("navbar-icon")
+// console.log(dropdown)
 
+// function iconAddX() {
+//   const dropdown = document.getElementsByClassName("fa-bars");
+
+  
+//   const boxPressed = (e) => {
+//     e.target.className = "fa-solid fa-xmark";
+//     console.log(e.target.value)
+//   }
+//   dropdown[0].addEventListener("click", boxPressed);
+// }
+
+// iconAddX()
+// function iconAddBar() {
+//   const dropdown = document.getElementsByClassName("fa-xmark");
+
+  
+//   const boxPressed = (e) => {
+//     e.target.className = "fa-solid fa-bars";
+//     console.log(e.target.value)
+//   }
+//   dropdown[0].addEventListener("click", boxPressed);
+// }
+
+// iconAddBar()
+
+function switchIcon(x) {
+  x.classList.toggle("fa-xmark")
+}
+
+// const handleBtnClick = e => {
+//   if(e.target.innerText === "On") {
+//     e.target.innerText = "Off"
+//   } else {
+//     e.target.innerText = "On"
+//   }
+// }
+// dropdown.addEventListener("click",dropdownChild )
