166b7a9c492f77e6b793457368205fc5a7fe5ad2
Jake Egbert <jake.egbert04@gmail.com>
2023-03-20 16:50:37 -0600

    finished assignment

--- counter-widget/script.js
+++ counter-widget/script.js
@@ -1,8 +1,33 @@
-/* 
-Counter Widget
- - Header
- - Current count (start at 0)
- - Three buttons: decrease, reset, increase, that will update the count and render to the dom respectively.
- - Current count should black if the count is 0. If it's in the positive, the count should be green, negative should be red.
- - Style everything
-*/
+
+let data = 0;
+const number = document.getElementById("number-int")
+
+
+number.innerText = data;
+
+function increase() {
+  number.innerText = ++data;
+  NumColor()
+}
+function decrease() {
+  number.innerText = --data;
+  NumColor()
+}
+function resetNum() {
+  data = 0;
+  number.innerText = data;
+  NumColor()
+}
+
+function NumColor() {
+  if(data == 0) {
+    number.style.color = "black"
+  } else if(data < 0) {
+    number.style.color = "red"
+  } else {
+    number.style.color = "green"
+  }
+}
+
+
+
