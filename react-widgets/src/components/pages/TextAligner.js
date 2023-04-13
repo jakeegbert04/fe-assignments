691f017e4ff885467622b358b2803a9a489f7616
Jake Egbert <jake.egbert04@gmail.com>
2023-04-12 20:55:41 -0600

    finsihed text align midget

--- react-widgets/src/components/pages/TextAligner.js
+++ react-widgets/src/components/pages/TextAligner.js
@@ -5,8 +5,13 @@ export default function TextAligner() {
   const [text, setText] = useState("center")
 
   return (
-    <div className="main-container text-align-container" style={{textAlign: "center"}}>
-      hello
+    <div className="main-container text-align-container">
+      <div className="display" style={{textAlign: `${text}`}} >Push Me Around</div>
+      <div className="buttons">
+        <button onClick={() => setText((text) => text = "left")}>Left</button>
+        <button onClick={() => setText((text) => text = "center")}>Center</button>
+        <button onClick={() => setText((text) => text = "right")}>Right</button>
+      </div>
     </div>
   )
 }
